e8d8b4296abf258a6e4d2f068a1d778b5bb748bd
daoswap002 <daoswap@daoswap.cc>
2021-07-26 13:49:56 +0800

    init project

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const CommunityRewardsContractAddress =
+  "0xa51f8BF280914b4754ef711C10A2CeEb938B473B";
+
+export const DAOAddress = "0xc096332CAacF00319703558988aD03eC6586e704";
+
+export const ZeroAddress = "0x0000000000000000000000000000000000000000";
+
+export const CHAIN_ID = process.env.VUE_APP_CHAIN_ID || 128;
+
+export const NETWORK_ID = process.env.VUE_APP_NETWORK_ID || 128;
