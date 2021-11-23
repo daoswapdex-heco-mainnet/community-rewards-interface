9afe8d6da10273cff1f2d21ccd2414921ae430d4
daoswap002 <daoswap@daoswap.cc>
2021-11-23 09:13:57 +0800

    add DST

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,9 +4,14 @@ export const CommunityRewardsContractAddress =
 export const CommunityRewardsDATContractAddress =
   "0x6Ef03c4E119C1f5BBc0430c0672a0579f9fd045C";
 
+export const CommunityRewardsDSTApproveContractAddress =
+  "0x6A1257C6370cc962281E0550114379942Da95f97";
+
 export const DAOAddress = "0xc096332CAacF00319703558988aD03eC6586e704";
 
 export const DATAddress = "0x738B815eaDD06E0041b52B0C9d4F0d0D277B24bA";
+
+export const DSTAddress = "0xae21384FafC42FB8FA25706CB107185a98d5CB57";
 
 export const ZeroAddress = "0x0000000000000000000000000000000000000000";
 
